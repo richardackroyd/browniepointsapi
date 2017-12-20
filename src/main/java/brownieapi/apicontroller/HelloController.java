fdb01801b11c43a8d1ad82ab87114ac84f7f46d3
RICHACKROYD <richard.ackroyd@live.co.uk>
2017-12-20 18:14:03 +0100

    additional changes for h2

--- src/main/java/brownieapi/apicontroller/HelloController.java
+++ src/main/java/brownieapi/apicontroller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot and Rich!";
+        return "Greetings from Spring Boot and Rich with profiles!";
     }
 
 }
